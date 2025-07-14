d3f31b482f4f84b77ae1bef9c24659a2cf68c93c
Matheus Faria de Carvalho <matheusfariadecarvalho@example.com>
2025-07-14 16:01:09 -0300

    build: adiciona pasta docs para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    build: {
-    outDir: 'docs'
+  base: "./",
+  build: {
+    outDir: "docs",
   },
-    plugins: [react()],
+  plugins: [react()],
 });
