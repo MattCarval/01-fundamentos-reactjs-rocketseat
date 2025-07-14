195bc8b9f4fc0809308b847bc53e1c150002c48b
Matheus Faria de Carvalho <matheusfariadecarvalho@example.com>
2025-07-14 15:54:52 -0300

    change build output

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  if (command === "serve") {
-    return {
-      base: "/",
-      plugins: [react()],
-    };
-  } else {
-    return {
-      base: "/01-fundamentos-reactjs-rocketseat/",
-      plugins: [react()],
-    };
-  }
+export default defineConfig({
+    build: {
+    outDir: 'docs'
+  },
+    plugins: [react()],
 });
